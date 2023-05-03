f69e3c314b6eaa5594bd436354b7f3c776f991d3
Ivan Belov <bis80@bk.ru>
2023-05-03 10:58:14 +0300

    add lists for buildings table

---
+++ resources/js/cps_portal_table/ag_grid_classes/lists.js
@@ -0,0 +1,16 @@
+let lists = {
+    buildings: {
+        area: ['ГП','Ямбург','ПС САП','Новый Уренгой'],
+        buildings:undefined,
+        group_1:undefined,
+        group_2:undefined,
+        queue: ['','1','2'],
+        affiliate:undefined,
+        aud_warn_type: ['1','2','3'],
+        type_aups: ['АСПС','АСПС, АСПТ','АСПТ','Противопожарный водопровод','СОУЭ'],
+        categ_asu: ['1','2','3'],
+    },
+    equipment: {
+
+    }
+}
