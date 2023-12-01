bb1fa389c912119e08f5617493c32ba8173f1fef
Ivan Belov <bis80@bk.ru>
2023-12-01 09:44:06 +0500

    fix 120 second to 2 hours

--- resources/js/cps_portal_table/idle-timer.js
+++ resources/js/cps_portal_table/idle-timer.js
@@ -15,7 +15,7 @@ export default class IdleTimer {
     }
 
     setIDLE_timer() {
-        this.IDLE_timerID = setTimeout(this.logOut, 120000);
+        this.IDLE_timerID = setTimeout(this.logOut, 720000);
     }
 
     logOut() {
