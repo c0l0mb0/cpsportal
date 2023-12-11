34f1481a94fd26846565580d5e8a0c96f9a66ad0
Ivan Belov <bis80@bk.ru>
2023-12-11 09:28:30 +0500

    add affiliate field to plangraf edit

--- resources/js/cps_portal_table/ag-grid-parameters.js
+++ resources/js/cps_portal_table/ag-grid-parameters.js
@@ -10,113 +10,113 @@ import {userRole} from "./app";
 export let agGridParameters = {
     agOuterId: undefined,
     actionMenu: undefined,
-    workersParameters: {
-        gridOptions: {
-            columnDefs: [
-                {headerName: "ФИО", field: "fio", minWidth: 100, tooltipField: 'fio', sortable: true},
-                {
-                    headerName: "N табеля",
-                    field: "tab_nom",
-                    minWidth: 100,
-                    tooltipField: 'tab_nom',
-                    cellEditor: NumericCellEditor,
-                },
-                {headerName: "Должность", field: "worker_position", minWidth: 100, tooltipField: 'worker_position'},
-            ],
-            rowSelection: 'single',
-            defaultColDef: {
-                resizable: true,
-                editable: true,
-                menuTabs: ['filterMenuTab'],
-            },
-            enableBrowserTooltips: true,
-            onCellValueChanged: function (event) {
-                httpRequest(config.api.postPutDeleteWorkers, "PUT",
-                    addCSRF(event.data), event.data.id).catch((rejected) => console.log(rejected));
-            },
-            onRowSelected: function () {
-                agGridParameters.actionMenu.showDelButton();
-            },
-            onFirstDataRendered: (params) => {
-                params.api.sizeColumnsToFit();
-            }
-        },
-        agName: 'workers',
-    },
-    fireInstrParameters: {
-        gridOptions: {
-            columnDefs: [
-                {
-                    headerName: "ФИО",
-                    field: "fio",
-                    minWidth: 130,
-                    tooltipField: 'fio',
-                    editable: false,
-                    sortable: true
-                },
-                {
-                    headerName: "N табеля",
-                    field: "tab_nom",
-                    minWidth: 40,
-                    tooltipField: 'tab_nom',
-                    editable: false,
-                },
-                {
-                    headerName: "Должность",
-                    field: "worker_position",
-                    minWidth: 100,
-                    tooltipField: 'worker_position',
-                    editable: false,
-                },
-                {
-                    headerName: "ПоследняяПроверка",
-                    field: "fire_instr_last",
-                    minWidth: 60,
-                    tooltipField: 'fire_instr_last',
-                    cellEditor: DatePicker,
-                    valueFormatter: (params) => {
-                        if (params.data.fire_instr_last !== undefined && params.data.fire_instr_last !== null) {
-                            let dateAsString = params.data.fire_instr_last;
-                            let dateParts = dateAsString.split('-');
-                            return `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`;
-                        }
-                    },
-                },
-                {
-                    headerName: "СледующаяПроверка",
-                    field: "fire_instr_next",
-                    minWidth: 60,
-                    tooltipField: 'fire_instr_next',
-                    cellEditor: DatePicker,
-                    valueFormatter: (params) => {
-                        if (params.data.fire_instr_next !== undefined && params.data.fire_instr_next !== null) {
-                            let dateAsString = params.data.fire_instr_next;
-                            let dateParts = dateAsString.split('-');
-                            return `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`;
-                        }
-
-                    },
-                },
-            ],
-            rowSelection: 'single',
-            defaultColDef: {
-                resizable: true,
-                editable: true,
-                menuTabs: ['filterMenuTab'],
-            },
-            enableBrowserTooltips: true,
-            onCellValueChanged: function (event) {
-                httpRequest(config.api.postPutDeleteWorkers, "PUT", addCSRF(event.data), event.data.id).catch((rejected) => console.log(rejected));
-            },
-            onRowSelected: function () {
-                agGridParameters.actionMenu.showPlusSixButton();
-            },
-            onFirstDataRendered: (params) => {
-                params.api.sizeColumnsToFit();
-            }
-        },
-        agName: 'fireInstr',
-    },
+    // workersParameters: {
+    //     gridOptions: {
+    //         columnDefs: [
+    //             {headerName: "ФИО", field: "fio", minWidth: 100, tooltipField: 'fio', sortable: true},
+    //             {
+    //                 headerName: "N табеля",
+    //                 field: "tab_nom",
+    //                 minWidth: 100,
+    //                 tooltipField: 'tab_nom',
+    //                 cellEditor: NumericCellEditor,
+    //             },
+    //             {headerName: "Должность", field: "worker_position", minWidth: 100, tooltipField: 'worker_position'},
+    //         ],
+    //         rowSelection: 'single',
+    //         defaultColDef: {
+    //             resizable: true,
+    //             editable: true,
+    //             menuTabs: ['filterMenuTab'],
+    //         },
+    //         enableBrowserTooltips: true,
+    //         onCellValueChanged: function (event) {
+    //             httpRequest(config.api.postPutDeleteWorkers, "PUT",
+    //                 addCSRF(event.data), event.data.id).catch((rejected) => console.log(rejected));
+    //         },
+    //         onRowSelected: function () {
+    //             agGridParameters.actionMenu.showDelButton();
+    //         },
+    //         onFirstDataRendered: (params) => {
+    //             params.api.sizeColumnsToFit();
+    //         }
+    //     },
+    //     agName: 'workers',
+    // },
+    // fireInstrParameters: {
+    //     gridOptions: {
+    //         columnDefs: [
+    //             {
+    //                 headerName: "ФИО",
+    //                 field: "fio",
+    //                 minWidth: 130,
+    //                 tooltipField: 'fio',
+    //                 editable: false,
+    //                 sortable: true
+    //             },
+    //             {
+    //                 headerName: "N табеля",
+    //                 field: "tab_nom",
+    //                 minWidth: 40,
+    //                 tooltipField: 'tab_nom',
+    //                 editable: false,
+    //             },
+    //             {
+    //                 headerName: "Должность",
+    //                 field: "worker_position",
+    //                 minWidth: 100,
+    //                 tooltipField: 'worker_position',
+    //                 editable: false,
+    //             },
+    //             {
+    //                 headerName: "ПоследняяПроверка",
+    //                 field: "fire_instr_last",
+    //                 minWidth: 60,
+    //                 tooltipField: 'fire_instr_last',
+    //                 cellEditor: DatePicker,
+    //                 valueFormatter: (params) => {
+    //                     if (params.data.fire_instr_last !== undefined && params.data.fire_instr_last !== null) {
+    //                         let dateAsString = params.data.fire_instr_last;
+    //                         let dateParts = dateAsString.split('-');
+    //                         return `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`;
+    //                     }
+    //                 },
+    //             },
+    //             {
+    //                 headerName: "СледующаяПроверка",
+    //                 field: "fire_instr_next",
+    //                 minWidth: 60,
+    //                 tooltipField: 'fire_instr_next',
+    //                 cellEditor: DatePicker,
+    //                 valueFormatter: (params) => {
+    //                     if (params.data.fire_instr_next !== undefined && params.data.fire_instr_next !== null) {
+    //                         let dateAsString = params.data.fire_instr_next;
+    //                         let dateParts = dateAsString.split('-');
+    //                         return `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`;
+    //                     }
+    //
+    //                 },
+    //             },
+    //         ],
+    //         rowSelection: 'single',
+    //         defaultColDef: {
+    //             resizable: true,
+    //             editable: true,
+    //             menuTabs: ['filterMenuTab'],
+    //         },
+    //         enableBrowserTooltips: true,
+    //         onCellValueChanged: function (event) {
+    //             httpRequest(config.api.postPutDeleteWorkers, "PUT", addCSRF(event.data), event.data.id).catch((rejected) => console.log(rejected));
+    //         },
+    //         onRowSelected: function () {
+    //             agGridParameters.actionMenu.showPlusSixButton();
+    //         },
+    //         onFirstDataRendered: (params) => {
+    //             params.api.sizeColumnsToFit();
+    //         }
+    //     },
+    //     agName: 'fireInstr',
+    // },
     buildingsParameters: {
         gridOptions: {
             columnDefs: [
@@ -417,6 +417,15 @@ export let agGridParameters = {
                             field: "queue",
                             minWidth: 100,
                             tooltipField: 'queue',
+                            filter: true,
+                            editable: false,
+
+                        },
+                        {
+                            headerName: "Филиал",
+                            field: "affiliate",
+                            minWidth: 100,
+                            tooltipField: 'affiliate',
                             filter: true,
                             editable: false,
 
